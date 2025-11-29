3d50433bcf408322b070622f3a154a284fc8f324
smegasberla <manu.riefolo.10@gmail.com>
2025-11-29 19:52:24 +0100

    Fix line endings and permissions for build script

--- build-linux.sh
+++ build-linux.sh
@@ -1,13 +1,25 @@
-#!/bin/bash
+﻿#!/bin/bash
+
+
 
 echo "Building Linux AppImage..."
 
+
+
 # Build the Docker image
+
 docker build -t flowgui-linux-builder -f Dockerfile.linux .
 
+
+
 # Create a container and copy the built files
+
 docker create --name flowgui-builder flowgui-linux-builder
+
 docker cp flowgui-builder:/app/dist-electron ./
+
 docker rm flowgui-builder
+
+
 
 echo "Linux build complete! Check the dist-electron folder for the AppImage."
